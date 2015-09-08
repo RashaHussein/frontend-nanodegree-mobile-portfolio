b456c92a91558814bdb7dff2fe2c37ed69242f3e
Rasha Khatib <rasharasem@gmail.com>
2015-09-07 21:04:04 -0700

    Optimization for index.html and automating some tasks using grunt tools

---
+++ Gruntfile.js
@@ -0,0 +1,155 @@
+'use strict';
+
+var ngrok = require('ngrok');
+var compression = require('compression');
+
+module.exports = function(grunt) {
+	// Load grunt tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Grunt configuration
+	grunt.initConfig({
+		pagespeed: {
+			options: {
+				nokey: true,
+				locale: 'en_GB',
+				threshold: 90
+			},
+			local: {
+				options: {
+					strategy: 'desktop'
+				}
+			},
+			mobile: {
+				options: {
+					strategy: 'mobile'
+				}
+			},
+			 paths: {
+		    options: {
+		      paths: ["/views/pizza.html"],
+		      locale: "en_GB",
+		      strategy: "desktop",
+		      threshold: 80
+		    }
+		  }
+		},
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            'dist/*',
+            '!dist/.git*'
+          ]
+        }]
+      },
+    },
+		imagemin: {
+			dist: {
+			  options: {
+			  	optimizationLevel: 7
+			  },
+			  files: [{
+					expand: true,
+					cwd: 'img',
+					src: ['*.{png,jpg}'],
+					dest: 'dist/img'
+			  }]
+			}
+		},
+		cssmin: {
+			dist: {
+			  files: [{
+			    expand: true,
+		      src: ['css/*.css'],
+		      dest: 'dist',
+		      ext: '.css'
+			  }]
+			}
+		},
+		uglify: {
+			dist: {
+			  files: [{
+			    expand: true,
+		      src: ['js/*.js'],
+		      dest: 'dist',
+		      ext: '.js'
+			  }]
+			}
+		},
+    htmlmin: {
+      dist: {
+        files: [{
+          expand: true,
+          src: ['*.html'],
+          dest: 'dist'
+        }]
+      }
+    },
+    assets_inline: {
+	    all: {
+	      options: {
+	        minify: true
+	      },
+	      files: {
+	        'dist/index.html': 'index.html',
+	        'dist/project-2048.html': 'project-2048.html',
+	        'dist/project-mobile.html': 'project-mobile.html',
+	        'dist/project-webperf.html': 'project-webperf.html'
+	      },
+	    },
+	  },
+    connect: {
+    	dev: {
+	      options: {
+	        port: 8080,
+	        hostname: '0.0.0.0',
+      		keepalive: true
+	      }
+    	},
+      prod: {
+        options: {
+	        port: 8080,
+	        hostname: '0.0.0.0',
+          open: true,
+          base: 'dist',
+      		keepalive: true,
+	        middleware: function (connect, options, middlewares) {
+	          middlewares.unshift(compression());
+	          return middlewares;
+	        }
+        }
+      }
+    }
+	});
+
+	 // Register customer task for ngrok
+  grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function() {
+    var done = this.async();
+    var port = 8080;
+
+    ngrok.connect(port, function(err, url) {
+      if (err !== null) {
+        grunt.fail.fatal(err);
+        return done();
+      }
+      grunt.config.set('pagespeed.options.url', url);
+      grunt.task.run('pagespeed');
+      done();
+    });
+  });
+
+	// Register default tasks
+	grunt.registerTask('default', 'connect:prod');
+  grunt.registerTask('perf', ['psi-ngrok']);
+  grunt.registerTask('build', [
+  	'clean:dist',
+  	'htmlmin',
+  	'cssmin',
+  	'uglify',
+  	'assets_inline',
+  	'imagemin'
+  ]);
+};
